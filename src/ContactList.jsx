5ba74c464eb30153360c63f59dfb68128a4e7438
guru-sode <gurukiran.sd@mountblue.io>
2018-09-27 15:47:01 +0530

    Add router functionality

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
 import data from './data/contactList.json';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 
 class ContactList extends Component {
-    constructor(){
-        super();
-        this.state ={
-            name:[]
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: []
+    };
+  }
 
-    componentDidMount = () =>{
-        const personName=[];
-            data.map((person)=>personName.push(person.name));
-            this.setState({
-                name:personName
-            })
-    }
+  componentDidMount = () => {
+    const personName = [];
+    data.map(person => personName.push(person.name));
+    this.setState({
+      name: personName
+    });
+  };
 
-    renderList(){
-        const names=[];
-        for(let i=0;i<this.state.name.length;i++){
-             names.push((<li className='list-group-item' key={this.state.name[i]}>{this.state.name[i]}</li>));
-        }
-        return names;
+  renderList() {
+    const names = [];
+    for (let i = 0; i < this.state.name.length; i++) {
+      names.push(
+      <NavLink to={`/${this.state.name[i]}`}>
+              <li
+          className="list-group-item"
+          onClick={this.handleClick.bind(this, this.state.name[i])}
+          key={this.state.name[i]}
+        >
+          {this.state.name[i]}
+        </li>
+       </NavLink> 
+      );
     }
-    render() {
-        console.log(this.state.name);
-        return (
-            <div>
-                <h1>Contact Manager</h1>
-                <div className='contact-container'>
-                <h3>Contacts</h3>
-                <ul className='list-group'>{this.renderList()}</ul>
-                </div>
-            </div>
-        );
-    }
+    return names;
+  }
+  handleClick(key){
+
+  }
+
+
+  render() {
+    return (
+        <BrowserRouter>
+      <div>
+        <h1>Contact Manager</h1>
+        <div className="contact-container">
+          <h3>
+            Contacts <i className="fa fa-plus-circle" />
+          </h3>
+          <ul className="list-group">{this.renderList()}</ul>
+        </div>
+        <div className="conatct-details-container">
+        <h3>Contact details</h3>
+        <Route path='/Gurukiran' render={()=>{
+            return <h1>Hello</h1>
+        }}></Route>
+        </div>
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default ContactList;
