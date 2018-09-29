72bda03754a04c5ab1cc9eb306fbf190a8ed1514
guru-sode <gurukiran.sd@mountblue.io>
2018-09-29 13:32:48 +0530

    Add edit and delete functionality

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import {Fragment} from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
-import {NavLink} from 'react-router-dom';
 import data from '../data/contactList.json';
 import '../styles/contactCard.css';
+import ReactDOM from 'react-dom';
 
 class ContactDetails extends Component {
   constructor() {
@@ -17,7 +15,7 @@ class ContactDetails extends Component {
   }
 
   componentDidUpdate = () => {
-    const index = this.getIndex();
+    const index = this.getIndex(this.props.match.params.name);
     let name;
     let home;
     let mobile;
@@ -36,10 +34,29 @@ class ContactDetails extends Component {
       });
   };
 
-  getIndex() {
+  componentDidMount=()=>{
+    const index = this.getIndex(this.props.match.params.name);
+    let name;
+    let home;
+    let mobile;
+    let birthday;
+    let person = data[index];
+    name = person.name;
+    birthday = person.birthday;
+    home = person.numbers.home;
+    mobile = person.numbers.mobile;
+    this.setState({
+      name: name,
+      home: home,
+      mobile: mobile,
+      birthday: birthday
+    });
+  }
+
+  getIndex(name) {
     let index;
     data.map((person, i) => {
-      if (this.props.match.params.name === person.name) {
+      if (name === person.name) {
         index = i;
       }
       return index;
@@ -47,8 +64,42 @@ class ContactDetails extends Component {
     return index;
   }
 
+  editContact(){
+    console.log('submit');
+  }
+
+  handleChangeName=(event)=>{
+    console.log(event.target.value);
+    this.setState({
+      name:event.target.value
+    })
+  }
+
+  edit=()=>{
+    ReactDOM.render(<div className="container">
+    <form>
+      <div className="form-group">
+        <label>Name:</label>
+        <input type="name" className="form-control" id="name" placeholder="Enter name" name="name" defaultValue={this.state.name} onChange={this.handleChangeName} />
+      </div>
+      <div className="form-group">
+        <label>Birthday:</label>
+        <input type="birthday" className="form-control" id="birthday" placeholder="Enter birthday" name="birthday" defaultValue={this.state.birthday} onChange={this.handleChangeBirth} />
+      </div>
+      <div className="form-group">
+        <label>Home:</label>
+        <input type="home" className="form-control" id="home" placeholder="Enter home number" name="home" defaultValue={this.state.home} onChange={this.handleChangeHome}/>
+      </div>
+      <div className="form-group">
+        <label>Mobile:</label>
+        <input type="mobile" className="form-control" id="mobile" placeholder="Enter mobile number" name="mobile" defaultValue={this.state.mobile}  onChange={this.handleChangeMobile}/>
+      </div>
+      <button type="submit" className="btn btn-success" >Submit</button>
+    </form>
+  </div>,document.getElementById('edit-form'));
+  }
+
   render() {
-    //   console.log(this.props.match);
     return (
       <div id="vcard">
         <div id="card-content">
@@ -67,12 +118,8 @@ class ContactDetails extends Component {
             </span>
           </div>
         </div>
-        <BrowserRouter>
-        <Fragment>
-        <NavLink to={`${this.props.match.url}/delete`}><button type="button" className="btn btn-danger">Delete</button></NavLink>
-        <NavLink to={`${this.props.match.url}/edit`}><button type="button" className="btn btn-warning">Edit</button></NavLink>
-        </Fragment>
-        </BrowserRouter>
+      <button type="button" className="btn btn-warning" onClick={this.edit}>Edit</button>
+      <div id="edit-form"></div>
       </div>
     );
   }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import ContactDetails from './ContactDetails';
 
+
 class ContactList extends Component {
   constructor() {
     super();
@@ -23,18 +24,42 @@ class ContactList extends Component {
     });
   };
 
+  handleDelete(deleteName){
+    const index = this.getIndex(deleteName);
+    this.state.name.splice(index,1);
+    console.log(this.state.name);
+    this.setState({
+      name:this.state.name
+    });
+
+  }
+
+  getIndex(name) {
+    let index;
+    data.map((person, i) => {
+      if (name === person.name) {
+        index = i;
+      }
+      return index;
+    });
+    return index;
+  }
+
   renderList() {
     const names = [];
     for (let i = 0; i < this.state.name.length; i++) {
       names.push(
-        <NavLink to={`/${this.state.name[i]}`} key={this.state.name[i]}>
           <li className="list-group-item" key={this.state.name[i]}>
-            {this.state.name[i]}
+            <NavLink to={`/${this.state.name[i]}`} key={this.state.name[i]}>{this.state.name[i]}</NavLink>
+            <i className="fa fa-trash-o" onClick={this.handleDelete.bind(this,this.state.name[i])}  />
           </li>
-        </NavLink>
       );
     }
     return names;
+  }
+
+  createContact(){
+    console.log('create contact');
   }
 
   render() {
@@ -44,7 +69,7 @@ class ContactList extends Component {
           <h1>Contact Manager</h1>
           <div className="contact-container">
             <h3>
-              Contacts <i className="fa fa-plus-circle" />
+              Contacts <i className="fa fa-plus-circle" onClick={this.createContact}/>
             </h3>
             <ul className="list-group">{this.renderList()}</ul>
           </div>
